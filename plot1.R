74eff2ad3b2b66e2a5f4690cfa0113885ebdec80
David M <d.mendsev@gmail.com>
2020-08-31 21:40:51 +0200

    20200831_First commit

---
+++ plot1.R
@@ -0,0 +1,17 @@
+setwd("C:/Users/david/Desktop/R/ExData_Plotting1")
+library(dplyr)
+library(lubridate)
+
+## Read data, transform time variables into appropiate classes and subset the 
+## desired dates
+raw_data <- read.csv("household_power_consumption.txt",sep=";")
+raw_data$Date <- dmy(raw_data$Date)
+raw_data$Time <- hms(raw_data$Time)
+selected_data <-raw_data[(raw_data$Date==ymd("2007/02/01")|
+                                  raw_data$Date == ymd("2007/02/02")),]
+png(filename="plot1.png", width = 480, height = 480)
+hist(as.numeric(selected_data$Global_active_power), 
+     col="red", 
+     xlab="Global Active Power (kilowatts)", 
+     main="Global Active Power")
+dev.off()
